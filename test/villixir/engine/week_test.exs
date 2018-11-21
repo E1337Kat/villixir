8198ee7b295f58fdc7c9a13f209bbed8b4ce7aac
Ellie Peterson <ekp2h92@gmail.com>
2018-11-21 09:22:44 -0500

    Adding the base tests to look at based on the implementation process outlined.

---
+++ test/villixir/engine/week_test.exs
@@ -0,0 +1,5 @@
+defmodule Villixir.Engine.WeekTest do
+  use ExUnit.Case
+  doctest Villixir.Engine.Week
+
+end
--- test/villixir_test.exs
+++ test/villixir_test.exs
@@ -2,7 +2,21 @@ defmodule VillixirTest do
   use ExUnit.Case
   doctest Villixir
 
-  test "greets the world" do
-    assert Villixir.hello() == :world
+  test "creates the world" do
+    world = %{
+      world_width: 100,
+      world_height: 100,
+      buildings: [],
+      connections: [{"Javierville", "south"}, {"Capetown", "west"}]
+    }
+
+    connections = [
+      {"Javierville", "south"},
+      {"Capetown", "west"}
+    ]
+    difficulty = 1
+
+    assert Villixir.Engine.create_world(connections, difficulty) == world
   end
+
 end
