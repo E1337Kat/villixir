b57da0acf7c97578ea8f2f58570f40abeace7546
Ellie Peterson <ekp2h92@gmail.com>
2018-11-21 09:24:14 -0500

    Adding more tests.

--- test/villixir/engine/week_test.exs
+++ test/villixir/engine/week_test.exs
@@ -2,4 +2,25 @@ defmodule Villixir.Engine.WeekTest do
   use ExUnit.Case
   doctest Villixir.Engine.Week
 
+  test "week starts" do
+
+  end
+
+  test "it moves forward" do
+
+  end
+
+  test "it loops" do
+
+  end
+
+  test "it gathers income" do
+
+  end
+
+  test "it loses expenses" do
+
+  end
+
+  test "it "
 end
